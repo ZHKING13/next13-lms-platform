18b4b89d8da6847fa52356a126414e334b60d102
ZETREY <84082026+ZHKING13@users.noreply.github.com>
2024-04-26 14:08:19 +0000

    fixe:file uploading

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -6,23 +6,35 @@ import { UploadDropzone } from "@/lib/uploadthing";
 import { ourFileRouter } from "@/app/api/uploadthing/core";
 
 interface FileUploadProps {
-  onChange: (url?: string) => void;
-  endpoint: keyof typeof ourFileRouter;
+    onChange: (url?: string) => void;
+    endpoint: keyof typeof ourFileRouter;
+}
+type UploadFileResponse =
+    | { data: UploadData; error: null }
+    | { data: null; error: UploadError };
+
+type UploadData = {
+    key: string;
+    url: string;
+    name: string;
+    size: number;
 };
 
-export const FileUpload = ({
-  onChange,
-  endpoint
-}: FileUploadProps) => {
-  return (
-    <UploadDropzone
-      endpoint={endpoint}
-      onClientUploadComplete={(res) => {
-        onChange(res?.[0].url);
-      }}
-      onUploadError={(error: Error) => {
-        toast.error(`${error?.message}`);
-      }}
-    />
-  )
-}
+type UploadError = {
+    code: string;
+    message: string;
+    data: any;
+};
+export const FileUpload = ({ onChange, endpoint }: FileUploadProps) => {
+    return (
+        <UploadDropzone
+            endpoint={endpoint}
+            onClientUploadComplete={(res?) => {
+                onChange(res?.[0]?.url);
+            }}
+            onUploadError={(error: Error) => {
+                toast.error(`${error?.message}`);
+            }}
+        />
+    );
+};

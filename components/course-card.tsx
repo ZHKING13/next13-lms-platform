c59bcf57ee3355ca6604bff12ba95cd87ecf3b3f
ZETREY <84082026+ZHKING13@users.noreply.github.com>
2024-04-26 15:32:57 +0000

    update coursListe

--- components/course-card.tsx
+++ components/course-card.tsx
@@ -5,61 +5,108 @@ import { BookOpen } from "lucide-react";
 import { IconBadge } from "@/components/icon-badge";
 import { formatPrice } from "@/lib/format";
 import { CourseProgress } from "@/components/course-progress";
-
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Button } from "./ui/button";
 interface CourseCardProps {
-  id: string;
-  title: string;
-  imageUrl: string;
-  chaptersLength: number;
-  price: number;
-  progress: number | null;
-  category: string;
-};
+    id: string;
+    title: string;
+    imageUrl: string;
+    chaptersLength: number;
+    price: number;
+    progress: number | null;
+    category: string;
+}
 
 export const CourseCard = ({
-  id,
-  title,
-  imageUrl,
-  chaptersLength,
-  price,
-  progress,
-  category
+    id,
+    title,
+    imageUrl,
+    chaptersLength,
+    price,
+    progress,
+    category,
 }: CourseCardProps) => {
-  return (
-    <Link href={`/courses/${id}`}>
-      <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
-        <div className="relative w-full aspect-video rounded-md overflow-hidden">
-          <Image
-            fill
-            className="object-cover"
-            alt={title}
-            src={imageUrl}
-          />
-        </div>
-        <div className="flex flex-col pt-2">
-          <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">
-            {title}
-          </div>
-          <p className="text-xs text-muted-foreground">
-            {category}
-          </p>
-          <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
+    return (
+        // href={`/courses/${id}`}
+        <>
+            <Dialog>
+                <DialogTrigger asChild>
+                    <div>
+                        <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
+                            <div className="relative w-full aspect-video rounded-md overflow-hidden">
+                                <Image
+                                    fill
+                                    className="object-cover "
+                                    alt={title}
+                    src={imageUrl}
+                    
+                                />
+                            </div>
+                            <div className="flex flex-col pt-2">
+                                <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">
+                                    {title}
+                                </div>
+
+                                {/* <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
             <div className="flex items-center gap-x-1 text-slate-500">
               <IconBadge size="sm" icon={BookOpen} />
               <span>
                 {chaptersLength} {chaptersLength === 1 ? "Chapter" : "Chapters"}
               </span>
             </div>
-          </div>
-          {progress !== null ? (
-            <CourseProgress
-              variant={progress === 100 ? "success" : "default"}
-              size="sm"
-              value={progress}
-            />
-          ) : null}
-        </div>
-      </div>
-    </Link>
-  )
-}
+          </div> */}
+                                {progress !== null ? (
+                                    <CourseProgress
+                                        variant={
+                                            progress === 100
+                                                ? "success"
+                                                : "default"
+                                        }
+                                        size="sm"
+                                        value={progress}
+                                    />
+                                ) : null}
+                            </div>
+                        </div>
+                    </div>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[525px] text-white p-5">
+                    <DialogHeader>
+                        <div
+                            className="bg-[imageUrl] w-full aspect-video bg-no-repeat md:bg-cover object-contain bg-cover bg-center"
+                            style={{ backgroundImage: `url(${imageUrl})` }}
+                        ></div>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4 text-white">
+                        <div className="">
+                            <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">
+                                {title}
+                            </div>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Sapiente praesentium est
+                                officia! Maxime quia esse reiciendis error
+                                autem, quisquam quae exercitationem doloremque
+                                cupiditate alias quaerat consectetur? Qui, odio
+                                nostrum? Voluptates.
+                            </p>
+                        </div>
+                    </div>
+                    <DialogFooter>
+                        <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </>
+    );
+};
--- components/courses-list.tsx
+++ components/courses-list.tsx
@@ -24,10 +24,10 @@ export const CoursesList = ({
 }: CoursesListProps) => {
   console.log(items);
   return (
-    <div>
-      <div className="w-full">
-        {/* module */}
-        {/* <Accordion type="single" collapsible>
+      <div>
+          <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
+              {/* module */}
+              {/* <Accordion type="single" collapsible>
           {
             items.map((item, index) => (
               <AccordionItem key={index} value={`item-${index}`}>
@@ -41,24 +41,24 @@ export const CoursesList = ({
             ))
           }
         </Accordion> */}
-        {items.map((item) => (
-          <CourseCard
-            key={item.id}
-            id={item.id}
-            title={item.title}
-            imageUrl={item.imageUrl!}
-            chaptersLength={item.chapters.length}
-            price={item.price!}
-            progress={item.progress}
-            category={item?.category?.name!}
-          />
-        ))}
+              {items.map((item) => (
+                  <CourseCard
+                      key={item.id}
+                      id={item.id}
+                      title={item.title}
+                      imageUrl={item.imageUrl!}
+                      chaptersLength={item.chapters.length}
+                      price={item.price!}
+                      progress={item.progress}
+                      category={item?.category?.name!}
+                  />
+              ))}
+          </div>
+          {items.length === 0 && (
+              <div className="text-center text-sm text-muted-foreground mt-10">
+                  Oops aucun cours suivie pour l&apos;instant
+              </div>
+          )}
       </div>
-      {items.length === 0 && (
-        <div className="text-center text-sm text-muted-foreground mt-10">
-          Oops aucun cours suivie pour l&apos;instant
-        </div>
-      )}
-    </div>
-  )
+  );
 }
